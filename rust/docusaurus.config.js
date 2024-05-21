b207a4ea742f381698b6f7b6e90f1fc8ab5ba67f
chichun <chichunhuamg@gmail.com>
2024-05-21 16:09:47 +0800

    發布至 gitpages 所需相關設定:https://chichunhuang.github.io/vintage

--- rust/docusaurus.config.js
+++ rust/docusaurus.config.js
@@ -14,11 +14,13 @@ const config = {
 
   // Set the production url of your site here
      //*****TotemTemplate*****
-  url: 'https://rust.insect-totem.net',
+  url: 'https://chichunhuang.github.io',
+  //url: 'https://rust.insect-totem.net',
   
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/vintage/', // gitpages: set same as projectName 
+  //baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
