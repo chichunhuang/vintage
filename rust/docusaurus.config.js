cae1c2bec72612b9e48c90999f02f46d1a393bc8
chichun <chichunhuamg@gmail.com>
2024-05-22 12:52:20 +0800

    DNS

--- rust/docusaurus.config.js
+++ rust/docusaurus.config.js
@@ -19,8 +19,8 @@ const config = {
   
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vintage/', // gitpages: set same as projectName 
-  //baseUrl: '/',
+  //baseUrl: '/vintage/', // gitpages: set same as projectName 
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
